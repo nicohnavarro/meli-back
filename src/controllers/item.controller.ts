86a9a8be467415105675ddcdf9bfd86f781b1c0b
nicohnavarro <nicohnavarro@gmail.com>
2021-10-29 03:05:19 -0300

    :fire: Finish endpoints.

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -1,22 +1,34 @@
 import { Request, Response } from 'express'
-import { getSearchItems } from '../services/item.services';
-import { resultsToListItem } from '../utils/formaterData';
+import { getCategoryPathById, getItem, getItemDescription, getSearchCategory, getSearchItems } from '../services/item.services';
+import { formatItem, resultsToListItem } from '../utils/formaterData';
 import logger from '../utils/logger';
-
-type QueryParams = {
-  q: String,
-  l?: String
-}
+import { QueryParams } from '../utils/types';
 
 export async function searchItems(req: Request<{}, {}, {}, QueryParams>, res: Response) {
   try {
     const query = req.query.q
     const limit = req.query.l ? +req.query.l : 50
-    const items = await getSearchItems(query, limit)
-    if (!items)
+    const category = await getSearchCategory(query, limit);
+    const categoryPath = await getCategoryPathById(category.category_id);
+    const { data } = await getSearchItems(query, limit)
+    if (!data)
       return res.sendStatus(404);
-    const mappedItems = resultsToListItem(items.data.results);
+    const mappedItems = resultsToListItem(data.results, categoryPath.path_from_root);
     res.send(mappedItems);
+  }
+  catch (e: any) {
+    logger.error(e)
+    return res.status(409).send(e.message)
+  }
+}
+
+export async function getItemById(req: Request, res: Response) {
+  try {
+    const id: String = req.params.id;
+    const item = await getItem(id);
+    const itemDescription = await getItemDescription(id);
+    const itemCategory = await getCategoryPathById(item.category_id)
+    res.send(formatItem(item,true, itemDescription.plain_text,itemCategory.path_from_root))
   }
   catch (e: any) {
     logger.error(e)
